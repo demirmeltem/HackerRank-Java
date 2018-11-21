a3743db7c7309efe4c697fe296678673b055826e
demirmeltem <demirmeltem50@gmail.com>
2018-11-21 21:05:34 +0300

    Java Output Formatting Solution

--- src/hackerrank/OutputFormatting.java
+++ src/hackerrank/OutputFormatting.java
@@ -10,7 +10,14 @@ public class OutputFormatting {
         {
             String s1=sc.next();
             int x=sc.nextInt();
-            //Complete this line
+            //System.out.println(s1); --string
+            //System.out.println(x);  --int
+            System.out.printf("%-15s%03d%n", s1, x); //%n will give you the line ending --This is solution, others just for explanation
+            System.out.printf("%-15s", s1); //This is printing "java          ". Because -15 means, first print string after complete 15 character with space.
+            System.out.printf("%03d", x); //If the input java 50 then it prints "java          050". Why? This means, "03", the input should contains 3 characters if it is not then you complete with 0(start).
+            System.out.printf("%05d", x); //If the input 3 then it prints "00003".
+
+
         }
         System.out.println("================================");
 
